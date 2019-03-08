917110e3317777984d4b0b6b4833632f538bd3c5
LiulietLee <lee.liuliet@hotmail.com>
2019-03-08 21:45:30 +0800

    update migration

--- trunk/web/include/db_migration/migration_03072019.sql
+++ trunk/web/include/db_migration/migration_03072019.sql
@@ -2,3 +2,6 @@ use jol;
 
 alter table users add reading_authority smallint unsigned;
 alter table problem add reading_authority smallint unsigned;
+
+update problem set reading_authority=0;
+update users set reading_authority=0;
